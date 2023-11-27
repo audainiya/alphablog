d4f168241ea712d23b6b9f7118f685473fc02117
audainiya <audainiya@gmail.com>
2023-11-27 19:16:27 +0530

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles,only: [:show, :index]
+  resources :articles, except: [:destroy] # this will add all get request links automatically except destroy link
+
+  #post '/articles/new' => 'articles#create'
+  #post '/articles/:id' => 'articles#update'
+  #post '/articles/:id/edit' => 'articles#update' # this 3 lines are needed for other forms purpose
+
+  # we need this 2 lines for our delete link_to setup
+  delete 'articles/:id/delete' => 'articles#destroy', as: 'articles_delete'
+  get '/articles/:id/delete' => 'articles#destroy'
+  
 end
