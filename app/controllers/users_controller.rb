a2302029acf0610bbfc8599fe3038672c26cd15a
audainiya <audainiya@gmail.com>
2023-12-22 21:13:05 +0530

    using gravitar show users articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
+
     def new
         @user =User.new
     end
-    
+
+    def show
+        @user = User.find(params[:id])
+        #@articles =@user.articles
+    end
+
     def edit
         @user = User.find(params[:id])
     end
